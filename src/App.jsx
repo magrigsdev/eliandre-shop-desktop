b6ec216df348af1d5d73661308c0d4e255e4e12f
Eliandre <96908551+magrigsdev@users.noreply.github.com>
2025-12-10 12:09:57 +0100

    feat(bienvenue-page) application des routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,34 @@ import Navbar from './layouts/Navbar'
 import Accueil from './components/accueil'
 import Footer from './layouts/footer'
 import { NavbarProvider } from './context/navbarProvider'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Bienvenue from './components/bienvenue'
+import Connexion from './components/connexion'
+import Inscription from './components/inscription';
 
 
 
 function App() {
 
   return (
-        // <Bienvenue/>
-        // <Connexion/>
-      // <Inscription/>
-     
+        
       <>
+      
         <NavbarProvider>
-          <Navbar/>
-          <Accueil/>
-          <Footer/>
+
+          <BrowserRouter>
+            <Navbar/>
+            <Routes>
+            
+              <Route path="/" element={ <Bienvenue /> }  />
+              <Route path="/accueil" element={ <Accueil /> }  />
+              <Route path="/connexion" element={ <Connexion/> }  />
+              <Route path="/inscription" element={ <Inscription/> }  />
+
+            </Routes>
+            <Footer/>
+
+          </BrowserRouter>
         </NavbarProvider>     
       </>
   )
